d5cddbdcfc008433131d2bb6c19d34e907658eb2
shintaayuningtias09 <168724787+shintaayuningtias09@users.noreply.github.com>
2026-01-08 16:53:32 +0700

    Add files via upload

---
+++ Percabangan/ganjilgenap.java
@@ -0,0 +1,13 @@
+public class ganjilgenap {
+    public static void main(String[] args) {
+        int bilangan = 7;   
+
+        if (bilangan % 2 == 0){
+            System.out.println( bilangan + "adalah bilangan genap");
+        }
+        else {
+            System.out.println(bilangan + "adalah bilangan ganjil");
+        }
+
+    }
+}
